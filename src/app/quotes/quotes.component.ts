1381529bf13b58c5c0069a977d2922a000a5bad9
barackmaund1 <pmaundu49@gmail.co>
2020-03-27 23:02:22 +0300

    content added in the class

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,3 +1,4 @@
+import { Quotes } from './../quotes';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-
+  quotes:Quotes[]=[
+    new Quotes(1,'sweet love','your always gain by giving love',"Resse Withersporn","Max-well",new Date()),
+    new Quotes(2,'inspiration','You just can beat the person who never gives up','beinspired.com','John kipchoge',new Date()),
+    new Quotes(3,"Motivation","With the new day comes new strength and new thoughts","Eleoner.com","Atony litunda",new Date()),
+    new Quotes(4,'bible','Without holiness no one will see God:Heb 12:14','Prophet Edward Owuor','barack maundu',new Date())
+  ]
   constructor() { }
 
   ngOnInit(): void {
