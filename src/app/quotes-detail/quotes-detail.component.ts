1c036195f66040b63de10af9e4a0b01fcbfb1357
barackmaund1 <pmaundu49@gmail.co>
2020-03-28 09:40:44 +0300

    changed class property

--- src/app/quotes-detail/quotes-detail.component.ts
+++ src/app/quotes-detail/quotes-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Quotes } from './../quotes';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-quotes-detail',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes-detail.component.css']
 })
 export class QuotesDetailComponent implements OnInit {
+  @Input() quote:Quotes;
 
   constructor() { }
 
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,3 +1,6 @@
 export class Quotes {
-    constructor(public id:number,public name:string,public descrition:string,public author:string, public poster:string, public completeDate:Date){}
+    showDescription:boolean
+    constructor(public id:number,public name:string,public description:string,public author:string, public poster:string, public completeDate:Date){
+this.showDescription=false;
+    }
 }
