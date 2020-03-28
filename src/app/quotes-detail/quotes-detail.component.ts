eeb6027aa3ba42ffc464ddfffd1605c9c1753857
barackmaund1 <pmaundu49@gmail.co>
2020-03-28 12:15:54 +0300

    added pipe date

--- src/app/quotes-detail/quotes-detail.component.ts
+++ src/app/quotes-detail/quotes-detail.component.ts
@@ -1,5 +1,5 @@
 import { Quotes } from './../quotes';
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-quotes-detail',
@@ -8,7 +8,15 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class QuotesDetailComponent implements OnInit {
   @Input() quote:Quotes;
+  numberOflikes:number=0;
+  numberOfdislikes:number=0;
 
+  likebuttonclick(){
+    this.numberOflikes ++;
+  }
+  dislikebuttonclick(){
+    this.numberOfdislikes ++;
+  }
   constructor() { }
 
   ngOnInit(): void {
