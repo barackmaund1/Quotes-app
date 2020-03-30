142f960514c5d6f3e5b8779588d9c8205e6459db
barackmaund1 <pmaundu49@gmail.co>
2020-03-30 09:02:21 +0300

    added number of upvotes

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
   quotes:Quotes[]=[
-    new Quotes(1,'Sweet love','your always gain by giving love',"Resse Withersporn","Max-well",new Date(2020,3,25),0,0),
-    new Quotes(2,'Inspiration',"You just can't beat the person who never gives up",'beinspired.com','John kipchoge',new Date(2019,4,6),0,0),
-    new Quotes(3,"Motivation","With the new day comes new strength and new thoughts","Eleoner.com","Atony litunda",new Date(2018,7,20),0,0),
-    new Quotes(4,'Bible','Without holiness no one will see God:Heb 12:14','Prophet Edward Owuor','barack maundu',new Date(2020,1,21),0,0)
+    new Quotes(1,'Sweet love','your always gain by giving love',"Resse Withersporn","Max-well",new Date(2020,3,25),20,0),
+    new Quotes(2,'Inspiration',"You just can't beat the person who never gives up",'beinspired.com','John kipchoge',new Date(2019,4,6),50,0),
+    new Quotes(3,"Motivation","With the new day comes new strength and new thoughts","Eleoner.com","Atony litunda",new Date(2018,7,20),56,0),
+    new Quotes(4,'Bible','Without holiness no one will see God:Heb 12:14','Prophet Edward Owuor','barack maundu',new Date(2020,1,21),50,0)
   ];
   toggleDetails(index){
     this.quotes[index].showDescription=!this.quotes[index].showDescription;
